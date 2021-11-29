2d2159a21986d2a03cc7198424f782ab7dc33075
chewbakai <kyrahceniza@gmail.com>
2021-11-29 23:34:01 +0800

    updated version

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
-const express = require("express");
-const cors = require("cors");
+const express = require("express"),
+    ejs = require('ejs');
+    app = express(),
+    path = require('path'),
+    fileUpload = require('express-fileupload'),
+    session = require("express-session"),
+    http = require("http"),
+    server = http.createServer(app),
+    bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}))  
 
-const app = express();
+app.set('view engine','ejs')
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname + '/public')));
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
-app.use(express.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+const tutorialRoutes = require("./app/routes/tutorialRoutes.js");
 
 // simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to WeTutor." });
-});
-
-require("./app/routes/tutorial.routes.js");
+app.get("/", (req, res) => res.send('index'))
+app.use(tutorialRoutes)
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
@@ -28,5 +26,4 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
 
-require("./app/routes/tutorial.routes.js")(app);
 
