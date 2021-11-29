2824388768d2b4de0d8a13bfdddd8bf760c96604
chewbakai <kyrahceniza@gmail.com>
2021-11-30 00:20:24 +0800

    updated version
    issue with public folder in routes path

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))  
 
 app.set('view engine','ejs')
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.static(path.join(__dirname + '/public')));
-
+app.set('views', path.join(__dirname, './app/views'));
+//app.use(express.static(path.join(__dirname + 'public')));
+app.use('/', express.static('public'));
 const tutorialRoutes = require("./app/routes/tutorialRoutes.js");
 
 // simple route
-app.get("/", (req, res) => res.send('index'))
-app.use(tutorialRoutes)
+app.get("/", (req, res) => res.render('index'))
+//app.use(tutorialRoutes)
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
