49156ae66f916cd51460c61d1920bfab07a788f6
chewbakai <kyrahceniza@gmail.com>
2021-12-01 18:39:41 +0800

    Revert "landing page"
    This reverts commit ecc80e96c94fa3fb3f93572866608aa2a11d8178.

--- app/routes/accountRoutes.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    const tutorials = require("../controllers/accountController.js");
-  
-    var router = require("express").Router();
-  
-    router.get("/register_account", accountController.registerAccount);
-    router.post("/login", accountController.loginAccount);
-  };
