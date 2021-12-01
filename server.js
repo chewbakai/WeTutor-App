9192d0a3afb80a4eb508bf2d54978c2b73801ea3
chewbakai <kyrahceniza@gmail.com>
2021-12-01 18:39:06 +0800

    Revert "updated version"
    This reverts commit 0b1ca9912efd40bfcbce6a6d376cb240732ad903.

--- server.js
+++ server.js
@@ -13,13 +13,13 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname, './app/views'));
 app.use(express.static(path.join(__dirname + 'public')));
+// app.use('/', express.static('public'));
 
 const tutorialRoutes = require("./app/routes/tutorialRoutes.js");
 
 // simple route
 app.get("/", (req, res) => res.render('index'))
 app.get("/class", (req,res) => res.render('class'))
-
 //app.use(tutorialRoutes)
 
 // set port, listen for requests
